7255568b45db8d1fc2e06b59c079eefe694befb3
Antonella Michelli <antonella.michelli@upplication.com>
2019-07-08 13:34:42 +0200

    Create card from fetch and basic styles

--- main.js
+++ main.js
@@ -10,9 +10,8 @@ function createRecipeCard(object) {
 
     const cardTitle = document.createTextNode(object.name);
     cardTitleEl.appendChild(cardTitle);
-    console.log(object.ingredients, object.ingredients.length)
 
-    for (let article of object.ingredients.length) {
+    for (let i = 0; i < object.ingredients.length; i++) {
 
         const articleContainerEl = document.createElement('div');
         const articleTitleEl = document.createElement('h3');
@@ -20,24 +19,43 @@ function createRecipeCard(object) {
         const articleWeightEl = document.createElement('p');
         const articleAmountEl = document.createElement('p');
         const articlePriceEl = document.createElement('p');
+        const articleLabelEl = document.createElement('label');
+        const articleCheckboxEl = document.createElement('input');
 
-        const articleTitle = document.createTextNode(article.product);
-        const articleBrand = document.createTextNode(article.brand);
-        const articleWeight = document.createTextNode(article.quantity);
-        const articleAmount = document.createTextNode(article.items);
-        const articlePrice = document.createTextNode(article.price);
+        articleContainerEl.classList.add('card__article__container');
+        articleTitleEl.classList.add('card__article__title');
+        articleBrandEl.classList.add('card__article__brand');
+        articleWeightEl.classList.add('card__article__weight');
+        articleAmountEl.classList.add('card__article__amount');
+        articlePriceEl.classList.add('card__article__price');
+        articleLabelEl.classList.add('visually-hidden');
+        articleLabelEl.for = `${object.ingredients[i].product}`;
+        articleLabelEl.setAttribute('data-price', `${object.ingredients[i].price}`)
+        articleCheckboxEl.type = 'checkbox';
+        articleCheckboxEl.id = `${object.ingredients[i].product}`;
+        articleCheckboxEl.classList.add('card__article__checkbox')
+
+        const articleTitle = document.createTextNode(object.ingredients[i].product);
+        const articleBrand = document.createTextNode(object.ingredients[i].brand);
+        const articleWeight = document.createTextNode(object.ingredients[i].quantity);
+        const articleAmount = document.createTextNode(object.ingredients[i].items);
+        const articlePrice = document.createTextNode(`${object.ingredients[i].price} €`);
+        const articleLabel = document.createTextNode(`${object.ingredients[i].product}`);
 
         articleTitleEl.appendChild(articleTitle);
         articleBrandEl.appendChild(articleBrand);
         articleWeightEl.appendChild(articleWeight);
         articleAmountEl.appendChild(articleAmount);
         articlePriceEl.appendChild(articlePrice);
+        articleLabelEl.appendChild(articleLabel);
 
         articleContainerEl.appendChild(articleTitleEl);
         articleContainerEl.appendChild(articleBrandEl);
         articleContainerEl.appendChild(articleWeightEl);
         articleContainerEl.appendChild(articleAmountEl);
         articleContainerEl.appendChild(articlePriceEl);
+        articleContainerEl.appendChild(articleLabelEl);
+        articleContainerEl.appendChild(articleCheckboxEl);
 
         cardArticleListEl.appendChild(articleContainerEl);
     }
@@ -45,7 +63,4 @@ function createRecipeCard(object) {
 
 fetch(rissotoRecipeURL)
     .then(res => res.json())
-    .then(data => {
-        console.log(data);
-        createRecipeCard(data.recipe);
-    })
+    .then(data => createRecipeCard(data.recipe))
