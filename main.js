bca9b8b3f6686627108cfb5b9feb7b7305c274ae
Antonella Michelli <antonella.michelli@upplication.com>
2019-07-09 09:21:31 +0200

    Finish shopping cart js logic

--- main.js
+++ main.js
@@ -4,7 +4,38 @@ const rissotoRecipeURL = 'https://raw.githubusercontent.com/Adalab/recipes-data/
 
 const cardTitleEl = document.querySelector('.card__title');
 const cardArticleListEl = document.querySelector('.card__article-list');
-const rissotoRecipe = '';
+let recipeCheckboxes = [];
+const totalItemsEl = document.querySelector('.card__items--total');
+const subtotalPriceEl = document.querySelector('.card__subtotal--total');
+const totalPriceEl = document.querySelector('.card__total--total');
+const shippingPriceEl = document.querySelector('.card__shipping-cost--total');
+let totalItems = 0;
+let totalPrice = 0;
+const shippingPrice = 7;
+setPurchaseValues();
+
+function setPurchaseValues() {
+    totalItemsEl.innerHTML = totalItems;
+    subtotalPriceEl.innerHTML = `${totalPrice.toFixed(2)} €`;
+    shippingPriceEl.innerHTML = `${shippingPrice.toFixed(2)} €`;
+    totalPriceEl.innerHTML = totalItems !== 0 ? `${(totalPrice + shippingPrice).toFixed(2)} €` : `${totalPrice.toFixed(2)} €`;
+}
+
+function handleRecipeCheckbox(event) {
+    const selectedCheckbox = event.currentTarget.checked;
+    const selectedItems = parseInt(document.querySelector(`p[data-amount=${event.currentTarget.id}]`).innerHTML);
+    const selectedPrice = parseFloat(document.querySelector(`p[data-price=${event.currentTarget.id}]`).innerHTML.replace(new RegExp(' €', 'g'), ''));
+
+    if (selectedCheckbox) {
+        totalItems += parseInt(selectedItems);
+        totalPrice += parseFloat(selectedPrice);
+    } else {
+        totalItems -= parseInt(selectedItems);
+        totalPrice -= parseFloat(selectedPrice);
+    }
+
+    setPurchaseValues();
+}
 
 function createRecipeCard(object) {
 
@@ -27,13 +58,15 @@ function createRecipeCard(object) {
         articleBrandEl.classList.add('card__article__brand');
         articleWeightEl.classList.add('card__article__weight');
         articleAmountEl.classList.add('card__article__amount');
+        articleAmountEl.setAttribute('data-amount', `${object.ingredients[i].product.replace(new RegExp(' ', 'g'), '_').replace(new RegExp(',', 'g'), '_').toLowerCase()}`);
         articlePriceEl.classList.add('card__article__price');
+        articlePriceEl.setAttribute('data-price', `${object.ingredients[i].product.replace(new RegExp(' ', 'g'), '_').replace(new RegExp(',', 'g'), '_').toLowerCase()}`);
+
         articleLabelEl.classList.add('visually-hidden');
         articleLabelEl.for = `${object.ingredients[i].product}`;
-        articleLabelEl.setAttribute('data-price', `${object.ingredients[i].price}`)
+        articleCheckboxEl.classList.add('card__article__checkbox');
         articleCheckboxEl.type = 'checkbox';
-        articleCheckboxEl.id = `${object.ingredients[i].product}`;
-        articleCheckboxEl.classList.add('card__article__checkbox')
+        articleCheckboxEl.id = `${object.ingredients[i].product.replace(new RegExp(' ', 'g'), '_').replace(new RegExp(',', 'g'), '_').toLowerCase()}`;
 
         const articleTitle = document.createTextNode(object.ingredients[i].product);
         const articleBrand = document.createTextNode(object.ingredients[i].brand);
@@ -59,6 +92,9 @@ function createRecipeCard(object) {
 
         cardArticleListEl.appendChild(articleContainerEl);
     }
+
+    recipeCheckboxes = document.querySelectorAll('.card__article__checkbox');
+    recipeCheckboxes.forEach(recipeCheckbox => recipeCheckbox.addEventListener('click', handleRecipeCheckbox));
 }
 
 fetch(rissotoRecipeURL)
