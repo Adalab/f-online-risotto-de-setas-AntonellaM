8fc5a51a2a67dce864c0b3b1ca3be7115f8f73da
Antonella Michelli <antonella.michelli@upplication.com>
2019-07-15 09:59:48 +0200

    Remove addAttributes and add SelectAll and UnselectAll

--- main.js
+++ main.js
@@ -4,15 +4,19 @@ const rissotoRecipeURL = 'https://raw.githubusercontent.com/Adalab/recipes-data/
 
 const cardTitleEl = document.querySelector('.card__title');
 const cardArticleListEl = document.querySelector('ul.container-fluid');
-let recipeCheckboxes = [];
 const totalItemsEl = document.querySelector('.card__items--total');
 const subtotalPriceEl = document.querySelector('.card__subtotal--total');
 const totalPriceEl = document.querySelector('.card__total--total');
 const shippingPriceEl = document.querySelector('.card__shipping-cost--total');
 const buttonPriceEl = document.querySelector('.card__total-price');
+const buttonSelectEl = document.querySelector('.card__button--select');
+const buttonUnselectEl = document.querySelector('.card__button--unselect');
+
+let recipeCheckboxes = [];
 let totalItems = 0;
 let totalPrice = 0;
 const shippingPrice = 7;
+
 setPurchaseValues();
 
 function setPurchaseValues() {
@@ -21,6 +25,27 @@ function setPurchaseValues() {
     shippingPriceEl.innerHTML = `${shippingPrice.toFixed(2)} €`;
     totalPriceEl.innerHTML = totalItems !== 0 ? `${(totalPrice + shippingPrice).toFixed(2)} €` : `${totalPrice.toFixed(2)} €`;
     buttonPriceEl.innerHTML = totalPriceEl.innerHTML;
+}
+
+function selectAllItems() {
+    const allItemsPrices = document.querySelectorAll('.card__article__price');
+    const allCheckboxes = document.querySelectorAll('input[type="checkbox"]');
+    console.log(allItemsPrices, allCheckboxes)
+    allCheckboxes.forEach(checkbox => checkbox.checked = true);
+    allItemsPrices.forEach(item => {
+        totalItems += 1;
+        totalPrice += parseFloat(item.innerHTML.replace(new RegExp(' €', 'g'), ''));
+        setPurchaseValues()
+    })
+}
+
+function resetShoppingList() {
+    const allCheckboxes = document.querySelectorAll('input[type="checkbox"]');
+    allCheckboxes.forEach(checkbox => checkbox.checked = false);
+
+    totalItems = 0;
+    totalPrice = 0;
+    setPurchaseValues();
 }
 
 function handleRecipeCheckbox(event) {
@@ -50,19 +75,10 @@ function addText(newElement, text) {
     newElement.appendChild(newText);
 }
 
-function addAttributes(newElement, attributes) {
-    console.log('fx', attributes, attributes[1][name], attributes[1].name)
-    for (let i = 0; i < attributes.length; i++) {
-        newElement.setAttribute(attributes[i][name], attributes[i].name);
-    }
-}
-
-function createElement(element, classes, text, attributes) {
+function createElement(element, classes, text) {
     const newElement = document.createElement(element);
-    console.log(attributes)
     classes && addClasses(newElement, classes);
     text && addText(newElement, text);
-    attributes && addAttributes(newElement, attributes)
 
     return newElement;
 }
@@ -71,6 +87,10 @@ function appendChildren(element, children) {
     for (let i = 0; i < children.length; i++) {
         element.appendChild(children[i]);
     }
+}
+
+function addEventListeners(elements, action, handler) {
+    elements.forEach(element => element.addEventListener(action, handler));
 }
 
 function createRecipeCard(object) {
@@ -85,15 +105,16 @@ function createRecipeCard(object) {
         //Checkbox
 
         const articleCheckboxContainerEl = createElement('div', ['d-flex', 'justify-content-center', 'align-items-center', 'col-1']);
-        const articleCheckboxEl = createElement('input', ['card__article__checkbox'], null, [{'type': 'checkbox'}, {'id': `${object.ingredients[i].product.replace(new RegExp(' ', 'g'), '_').replace(new RegExp(',', 'g'), '_').toLowerCase()}`}]);
-        // articleCheckboxEl.type = 'checkbox';
-        // articleCheckboxEl.id = `${object.ingredients[i].product.replace(new RegExp(' ', 'g'), '_').replace(new RegExp(',', 'g'), '_').toLowerCase()}`;
+        const articleCheckboxEl = createElement('input', ['card__article__checkbox']);
+        articleCheckboxEl.type = 'checkbox';
+        articleCheckboxEl.id = `${object.ingredients[i].product.replace(new RegExp(' ', 'g'), '_').replace(new RegExp(',', 'g'), '_').toLowerCase()}`;
         articleCheckboxContainerEl.appendChild(articleCheckboxEl);
 
         //Amount
 
         const articleAmountContainerEl = createElement('div', ['d-flex', 'justify-content-center', 'align-items-center', 'col-2']);
-        const articleAmountEl = createElement('p', ['card__article__amount', 'd-flex', 'justify-content-center', 'align-items-center'], object.ingredients[i].items, [{'data-amount': `${object.ingredients[i].product.replace(new RegExp(' ', 'g'), '_').replace(new RegExp(',', 'g'), '_').toLowerCase()}`}]); 
+        const articleAmountEl = createElement('p', ['card__article__amount', 'd-flex', 'justify-content-center', 'align-items-center'], object.ingredients[i].items);
+        articleAmountEl.setAttribute('data-amount', `${object.ingredients[i].product.replace(new RegExp(' ', 'g'), '_').replace(new RegExp(',', 'g'), '_').toLowerCase()}`);
         articleAmountContainerEl.appendChild(articleAmountEl);
 
         //Data
@@ -104,7 +125,8 @@ function createRecipeCard(object) {
         appendChildren(articleDataContainerEl, [articleTitleEl, articleBrandEl, articleWeightEl]);
         
         //Price
-        const articlePriceEl = createElement('p', ['card__article__price', 'col-2'], `${object.ingredients[i].price} €`, [{'data-price': `${object.ingredients[i].product.replace(new RegExp(' ', 'g'), '_').replace(new RegExp(',', 'g'), '_').toLowerCase()}`}]);
+        const articlePriceEl = createElement('p', ['card__article__price', 'col-2'], `${object.ingredients[i].price} €`);
+        articlePriceEl.setAttribute('data-price', `${object.ingredients[i].product.replace(new RegExp(' ', 'g'), '_').replace(new RegExp(',', 'g'), '_').toLowerCase()}`);
 
 
         appendChildren(articleContainerEl, [articleCheckboxContainerEl, articleAmountContainerEl, articleDataContainerEl, articlePriceEl])
@@ -113,9 +135,12 @@ function createRecipeCard(object) {
     }
 
     recipeCheckboxes = document.querySelectorAll('.card__article__checkbox');
-    recipeCheckboxes.forEach(recipeCheckbox => recipeCheckbox.addEventListener('click', handleRecipeCheckbox));
+    addEventListeners(recipeCheckboxes, 'click', handleRecipeCheckbox);
 }
 
 fetch(rissotoRecipeURL)
     .then(res => res.json())
     .then(data => createRecipeCard(data.recipe))
+
+buttonSelectEl.addEventListener('click', selectAllItems);
+buttonUnselectEl.addEventListener('click', resetShoppingList);
